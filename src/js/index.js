24164b267dfe5f292a229f557021e95e17f6f237
nowadraco <Nowadraco@gmail.com>
2023-01-22 01:00:51 +0000

    implementando javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
-const bntTrailer = getElementById('botao-trailer')
-const bntfechar = document.getElementById('btn-x');
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+
+const linkDoVideo = video.scr;
+
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.add("aberto");
+    video.setAttribute("src", "linkDoVideo");
+});
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "")
+});
 
